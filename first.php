09f056165644a45a45e487c5e1875fd83535580b
dieba17 <2020872786@student.uitm.edu.my>
2022-04-26 14:37:16 +0800

    first

--- first.php
+++ first.php
@@ -1,7 +1,15 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=p, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
 <?php
 echo "Hello world";
 ?>
+    <p><iframe src="http://localhost/Lab3/first.php" width="100%" height="700" style="border:none;"></iframe>/iframe></P></p>
 </body>
-</html> 
+</html>
